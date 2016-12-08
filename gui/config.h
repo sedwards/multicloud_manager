e782d91e11e4d03e40e60b7586517aa5476e1c19
Steven Edwards <winehacker@gmail.com>
2016-12-07 20:28:48 -0800

    Import gtk demos as a starting point

---
+++ gui/config.h
@@ -0,0 +1,312 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* The prefix for our gettext translation domains. */
+#define GETTEXT_PACKAGE "gtk30"
+
+/* Disable deprecation warnings from glib */
+#define GLIB_DISABLE_DEPRECATION_WARNINGS 1
+
+/* Define the location where the catalogs will be installed */
+#define GTK_LOCALEDIR "/Users/sedwards/gtk/inst/share/locale"
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* define if we have colord */
+/* #undef HAVE_COLORD */
+
+/* Define to 1 if you have the <crt_externs.h> header file. */
+#define HAVE_CRT_EXTERNS_H 1
+
+/* Define to 1 if CUPS 1.6 API is available */
+#define HAVE_CUPS_API_1_6 1
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the declaration of `isinf', and to 0 if you don't.
+   */
+#define HAVE_DECL_ISINF 1
+
+/* Define to 1 if you have the declaration of `isnan', and to 0 if you don't.
+   */
+#define HAVE_DECL_ISNAN 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `flockfile' function. */
+#define HAVE_FLOCKFILE 1
+
+/* Define to 1 if you have the <ftw.h> header file. */
+#define HAVE_FTW_H 1
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the `getresuid' function. */
+/* #undef HAVE_GETRESUID */
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define if gio-unix is available */
+#define HAVE_GIO_UNIX 1
+
+/* Have GNU ftw */
+/* #undef HAVE_GNU_FTW */
+
+/* Define to 1 if you have the `httpGetAuthString' function. */
+#define HAVE_HTTPGETAUTHSTRING 1
+
+/* Define if cups http_t authstring field is accessible */
+/* #undef HAVE_HTTP_AUTHSTRING */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if the system has the type `IPrintDialogCallback'. */
+/* #undef HAVE_IPRINTDIALOGCALLBACK */
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <linux/memfd.h> header file. */
+/* #undef HAVE_LINUX_MEMFD_H */
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the `localtime_r' function. */
+#define HAVE_LOCALTIME_R 1
+
+/* Define to 1 if you have the `lstat' function. */
+#define HAVE_LSTAT 1
+
+/* Define to 1 if you have the `mallinfo' function. */
+/* #undef HAVE_MALLINFO */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `mkstemp' function. */
+#define HAVE_MKSTEMP 1
+
+/* Define to 1 if you have a working `mmap' system call. */
+#define HAVE_MMAP 1
+
+/* Define to 1 if you have the `nearbyint' function. */
+#define HAVE_NEARBYINT 1
+
+/* Define to 1 if libpapi available */
+/* #undef HAVE_PAPI */
+
+/* Define to 1 if you have the `posix_fallocate' function. */
+/* #undef HAVE_POSIX_FALLOCATE */
+
+/* Have the Xrandr extension library */
+/* #undef HAVE_RANDR */
+
+/* Have the Xrandr 1.5 extension library */
+/* #undef HAVE_RANDR15 */
+
+/* Define to 1 if you have the `rint' function. */
+#define HAVE_RINT 1
+
+/* Define to 1 if you have the `round' function. */
+#define HAVE_ROUND 1
+
+/* Define to 1 if you have the `sincos' function. */
+/* #undef HAVE_SINCOS */
+
+/* Have the sockaddr_un.sun_len member */
+#define HAVE_SOCKADDR_UN_SUN_LEN 1
+
+/* Define to 1 if solaris xinerama is available */
+/* #undef HAVE_SOLARIS_XINERAMA */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#define HAVE_SYS_MMAN_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if sys/sysinfo.h is available */
+/* #undef HAVE_SYS_SYSINFO_H */
+
+/* Define to 1 if sys/systeminfo.h is available */
+/* #undef HAVE_SYS_SYSTEMINFO_H */
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Have the XCOMPOSITE X extension */
+/* #undef HAVE_XCOMPOSITE */
+
+/* Have the Xcursor library */
+/* #undef HAVE_XCURSOR */
+
+/* Have the XDAMAGE X extension */
+/* #undef HAVE_XDAMAGE */
+
+/* Have the XFIXES X extension */
+/* #undef HAVE_XFIXES */
+
+/* Define to 1 if XFree Xinerama is available */
+/* #undef HAVE_XFREE_XINERAMA */
+
+/* Have XGenericEvent */
+/* #undef HAVE_XGENERICEVENTS */
+
+/* Define to 1 if xinerama is available */
+/* #undef HAVE_XINERAMA */
+
+/* Define to use XKB extension */
+/* #undef HAVE_XKB */
+
+/* Have the SYNC extension library */
+/* #undef HAVE_XSYNC */
+
+/* Define to 1 if you have the `_lock_file' function. */
+/* #undef HAVE__LOCK_FILE */
+
+/* Define if _NL_MEASUREMENT_MEASUREMENT is available */
+/* #undef HAVE__NL_MEASUREMENT_MEASUREMENT */
+
+/* Define if _NL_PAPER_HEIGHT is available */
+/* #undef HAVE__NL_PAPER_HEIGHT */
+
+/* Define if _NL_PAPER_WIDTH is available */
+/* #undef HAVE__NL_PAPER_WIDTH */
+
+/* Define if _NL_TIME_FIRST_WEEKDAY is available */
+/* #undef HAVE__NL_TIME_FIRST_WEEKDAY */
+
+/* Define to 1 if you have the `_NSGetEnviron' function. */
+#define HAVE__NSGETENVIRON 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Define if <X11/extensions/XIproto.h> needed for xReply */
+/* #undef NEED_XIPROTO_H_FOR_XREPLY */
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://bugzilla.gnome.org/enter_bug.cgi?product=gtk%2B"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "gtk+"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "gtk+ 3.20.9"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gtk+"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.20.9"
+
+/* Use NSBundle functions to determine load paths for libraries, translations,
+   etc. */
+#define QUARTZ_RELOCATION 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if gmodule works and should be used */
+#define USE_GMODULE 1
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* Define to 1 if XInput 2.0 is available */
+/* #undef XINPUT_2 */
+
+/* Define to 1 if XInput 2.2 is available */
+/* #undef XINPUT_2_2 */
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* defines how to decorate public symbols while building */
+/* #undef _GDK_EXTERN */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
